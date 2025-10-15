0ca7a90c9e3099e677c58d5ee1362980f6a76bdd
FlavioKde <flaviodavirro@gmail.com>
2025-10-15 11:51:17 +0200

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.5.6"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.hackthon"
+version = "0.0.1-SNAPSHOT"
+description = "Demo project for Spring Boot"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation ("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+
+	// Data-BD
+	runtimeOnly("com.h2database:h2")
+	runtimeOnly ("mysql:mysql-connector-java")
+
+	// Swagger/OpenAPI
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.9")
+
+	// Logging
+	implementation ("org.springframework.boot:spring-boot-starter-logging")
+
+	// Testing
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/main/java/config/ApiConfig.java
@@ -0,0 +1,14 @@
+package config;
+
+public class ApiConfig {
+
+            private static final String COMMON_PATH = "/api";
+            private static final String API_VERSION = "/V0.1";
+            public static final String API_BASE_PATH = COMMON_PATH + API_VERSION;
+
+            public ApiConfig() {
+                throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
+            }
+
+
+}
