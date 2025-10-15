f0eb83baf04bfa0fbe48874f6cef8cfc59671fd6
FlavioKde <flaviodavirro@gmail.com>
2025-10-15 14:49:50 +0200

    add authorization for login

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,16 @@ dependencies {
 	runtimeOnly("com.h2database:h2")
 	runtimeOnly ("mysql:mysql-connector-java")
 
+	// Security
+	implementation("org.springframework.boot:spring-boot-starter-security")
+
+	// JWT
+	implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
+	implementation ("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+
+
 	// Swagger/OpenAPI
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.9")
 
